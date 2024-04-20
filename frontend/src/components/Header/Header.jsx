7414bf3a14c1a3acaf92cb647e19cbbdcdd143b4
Software Engineering Student <briksaliha@gmail.com>
2024-04-20 02:43:21 +0200

    update

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <div className="header">
+        <div className='header-contents'>
+            <h2>Order your Favorite Food here</h2>
+            <p>Choose your favorite food and enjoy your meal</p>
+            <button>Order Now</button>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Header
