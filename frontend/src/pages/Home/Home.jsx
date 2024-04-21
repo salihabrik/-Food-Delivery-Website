984d7ec8b46b54be56d30b46e0a322828851c272
Software Engineering Student <briksaliha@gmail.com>
2024-04-22 00:17:32 +0200

    update

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import './Home.css'
 import Header from '../../components/Header/Header'
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
 import FoodDisplay from '../../components/FoodDisplay/FoodDisplay'
+import  { useState } from 'react'
 
 const Home = () => {
 
