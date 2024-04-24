ec0edc63ba81c2e58064be03878e02de9e8d04a6
Software Engineering Student <briksaliha@gmail.com>
2024-04-24 18:37:02 +0200

    update

--- frontend/src/components/FoodItem/FoodItem.jsx
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -9,23 +9,14 @@ const FoodItem = ({ id, name, image, price, description }) => {
     <div className='food-item'>
       <div className='food-item-img-container'>
         <img className='food-item-image' src={image} alt='' />
-        
-          <div className='food-item-counter'>
-            <img
-              onClick={() => setItemCount(prev => prev - 1)}
-              src={assets.remove_icon_red}
-              alt=''
-            />
+        {!cartItems[id]
+         ?<img className='add' onClick={() => addToCart(id)} src={assets.add_icon_white} alt=''/>
+          :<div className='food-item-counter'>
+            <img onClick={() => removeFromCart(id)} src={assets.remove_icon_red} alt='' />
             <p>{itemCount}</p>
-            <img
-              onClick={() => setItemCount(prev => prev + 1)}
-              src={assets.add_icon_green}
-              alt=''
-            />
-            
-
+            <img onClick={() => addToCart(id)} src={assets.add_icon_green} alt='' />
           </div>
-        
+        }
       </div>
       <div className='food-item-info'>
         <div className='food-item-name-rating'>
