29ff4f56d97a6645c18d5de243c56563eb7de11b
Software Engineering Student <briksaliha@gmail.com>
2024-04-29 12:35:16 +0200

    update

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -11,7 +11,7 @@ const FoodDisplay = ({category}) => {
         <h2>Top dishes near you </h2>
         <div className='food-display-list' >
             {food_list.map((item,index)=> {
-              if (category=='All' || category===item.category) {
+              if (category==='All' || category===item.category) {
                 return <FoodItem key={index} id={item.id} name={item.name} image={item.image} price={item.price} description={item.description} />
               }    
                 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import {BrowserRouter} from 'react-router-dom'
-
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 import { StoreContextProvider } from './components/context/StoreContext';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-  <StoreContextProvider>
-    <App />
-  </StoreContextProvider>  
+    <StoreContextProvider>
+      <App />
+    </StoreContextProvider>
   </BrowserRouter>
-)
+);
