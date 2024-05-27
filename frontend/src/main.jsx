e1884b81ea3ab98a410c57ec83274459d27ce949
Software Engineering Student <briksaliha@gmail.com>
2024-05-27 15:41:33 +0200

    fix the files

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,18 +1,1 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
-import { BrowserRouter } from 'react-router-dom';
-import { StoreContextProvider } from 'src/components/Context/StoreContext.jsx';
 
-import { StrictMode } from 'react'; // Add this line
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <StrictMode> {/* Wrap the content with StrictMode */}
-    <BrowserRouter>
-      <StoreContextProvider>
-        <App />
-      </StoreContextProvider>
-    </BrowserRouter>
-  </StrictMode>
-);
